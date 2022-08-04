85ac09b7f6e91bdc57b4e9593ef43c90f4c0b2a5
godgodwinter <kakadz.pro@gmail.com>
2022-08-04 20:52:31 +0700

    add auto breadcum from route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 //codesandbox.io/s/github/tanstack/react-location/tree/main/examples/kitchen-sink?file=/src/index.tsx
 // https://react-location.tanstack.com/guides/route-loaders
-import { Link, Outlet, ReactLocation, Router } from "@tanstack/react-location";
+import {
+  Link,
+  Outlet,
+  ReactLocation,
+  Router,
+  useMatches,
+} from "@tanstack/react-location";
 import NavBar from "@/components/templates/landing/NavBarLanding";
 import Footer from "@/components/templates/FooterOne";
 
@@ -18,6 +24,9 @@ function App() {
           {
             path: "/",
             element: <Home />,
+            meta: {
+              breadcrumb: () => "Home",
+            },
           },
           {
             path: "posts",
@@ -25,15 +34,30 @@ function App() {
             pendingElement: async () => <div>Taking a long time...</div>,
             pendingMs: 1000 * 5, // 2 seconds
             pendingMinMs: 5000, // If it's shown, ensure the pending element is rendered for at least 500ms
+            meta: {
+              breadcrumb: () => "Posts",
+            },
           },
           {
             path: "/admin/pages/dashboard",
             element: <AdminLayout />,
+            meta: {
+              breadcrumb: () => "Admin",
+            },
             children: [
-              { path: "/", element: "Ini dashboard" },
+              {
+                path: "/",
+                element: "Ini dashboard",
+                meta: {
+                  breadcrumb: () => " Dashboard",
+                },
+              },
               {
                 path: ":postId",
                 element: "Ini Child",
+                meta: {
+                  breadcrumb: () => " Child",
+                },
               },
             ],
           },
@@ -44,6 +68,9 @@ function App() {
         </div>
         <hr />
         <div className="min-h-screen">
+          <div className="px-4 py-4">
+            <Breadcrumbs />
+          </div>
           <Outlet />
         </div>
         <Footer />
@@ -65,6 +92,9 @@ const AdminLayout = () => {
             <Link to="1" activeOptions={{ exact: true }}>
               Child
             </Link>
+            <Link to="/posts" activeOptions={{ exact: true }}>
+              Back to Posts Landing
+            </Link>
           </li>
         </ul>
       </div>
@@ -74,5 +104,30 @@ const AdminLayout = () => {
     </>
   );
 };
+function Breadcrumbs() {
+  const matches = useMatches();
+
+  return (
+    <div className="text-sm breadcrumbs">
+      <ul>
+        {matches
+
+          // skip routes that don't have a breadcrumb, like is the case of our '/' route
+
+          .filter((match) => (match.route ? match.route.meta.breadcrumb : null))
+
+          .map((match) => (
+            <li key={match.pathname}>
+              <Link to={match.pathname}>
+                {match.route.meta
+                  ? match.route.meta.breadcrumb(match.params)
+                  : null}
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
